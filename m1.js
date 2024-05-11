7cc0c1681411f120196bea3f5cd0c9aac4978803
hiten <hiteng53.com>
2024-05-11 19:17:58 +0530

    learn console log

---
+++ m1.js
@@ -0,0 +1,9 @@
+// window.alert("welcome to java script")
+// document.write("welcome to js")
+//  var x=12;
+// document.write("<br>"+x)
+// x="hellow";
+// var y=13
+// document.write("<br>"+x+y)
+document.getElementById("two").innerHTML="la la la"
+document.getElementById("one").innerHTML="lorem300"
---
+++ m2.js
@@ -0,0 +1,3 @@
+windows.alert("welcome to js")
+document.getElementById("one").innerHTML="welcome to js"
+document.getElementById("two").innerHTML="ha ha funny"
