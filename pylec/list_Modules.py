87def59f9cf02d54b514972e218b0cf6a9aed42b
hiten <hiteng53.com>
2024-06-15 18:46:43 +0530

    lo

--- pylec/list_Modules.py
+++ pylec/list_Modules.py
@@ -1,26 +1,25 @@
 l=[34,45,77,64,2,47]
 l1=[12,107,78]
-l.append(23)
+l.append(23)        #1
 print(l)
-l.sort()
+l.sort()            #2
 print(l)
-l.reverse()
+l.reverse()         #3
 print(l)
-l2:list[str]=l.copy()
+l2:list[str]=l.copy()           #4
 print(l2)
-l.reverse()
+l.reverse()         #5
 l.extend(l1)
 print(l)
-l.remove(45)
+l.remove(45)        #6
 print(l)
-y=l.index(23)
+y=l.index(23)       #7
 print(y)
-l[y]=32
+l[y]=32     
 print(l)
-print(l.count(45))
-z=l.pop(0)
+print(l.count(45))  #8
+z=l.pop(0)          #9
 print(z)
 print(l)
-l.clear()
+l.clear()           #10
 print(l)
-
