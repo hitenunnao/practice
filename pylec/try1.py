ed7ca736f0f673325e5d11ef232782fdeb5f76c7
hiten <hiteng53.com>
2024-07-07 12:45:17 +0530

    start learning new concepts

--- pylec/try1.py
+++ pylec/try1.py
@@ -1,25 +1,3 @@
-def nested1():
-    rows=int(input("enter rows: "))
-    column=int(input("enter columns: "))
-    symbol=input("enter symbol: ")
-    for i in range(rows):
-        for x in range(column):
-            print(symbol,end=" ")
-        print()
-nested1()
-
-def nested2():
-    symbol=input("enter symbol: ")
-    for i in range(1,5):
-        for x in range(0,i):
-            print(x,end=" ")
-        print()
-nested2()
-
-def nested3():
-    symbol=input("enter symbol: ")
-    for i in reversed(range(1,5)):
-        for x in range(0,i):
-            print(symbol,end=" ")
-        print()
-nested3()
+var = open('xnxx.txt','a')
+var1 = var.write("this  is ffirst line and tis is new text")
+var.close()
